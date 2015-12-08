7fd0de9508e52ffaaebc31f0f88e5a4fe9fa61fc
tcoach <admin@tcoach.com>
2015-12-08 09:47:36 -0600

    first

---
+++ attributes/default.rb
@@ -0,0 +1,6 @@
+default['awesome_customers']['user'] = 'web_admin'
+default['awesome_customers']['group'] = 'web_admin'
+
+default['awesome_customers']['document_root'] = '/var/www/customers/public_html'
+
+default['awesome_customers']['enabled_firewall_rules'] = %w(firewall_httpd firewall_sshd)
---
+++ recipes/default.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: awesome_customers
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+include_recipe 'selinux::permissive'
+include_recipe 'awesome_customers::user'
+include_recipe 'awesome_customers::webserver'
+include_recipe 'awesome_customers::firewall'
